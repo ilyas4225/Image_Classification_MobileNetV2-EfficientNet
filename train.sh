64e1e449f11d2502302ff0ddfac8eca9f2c0886d
ilyas4225 <muhammadilyas4225@gmail.com>
2023-02-27 22:03:18 +0500

    training

---
+++ train.sh
@@ -0,0 +1,3 @@
+python train.py --data /home/msiddi01/ImageNet_Classes_2/Volley_Basket/ --class_labels 0 1 --save EfficientNetB0_ImNetClass01
+python train.py --data /media/abdul/8266fdc5-61f6-47d6-9e82-aa0e56f4b319/Ilyas/classification/Benchmark-TinyNAS_EfficientNet_ImNet_Vehicles/data/food-101.tar.gz --dataset Food101 --epochs 3  --save EfficientNetB0_food101
+
