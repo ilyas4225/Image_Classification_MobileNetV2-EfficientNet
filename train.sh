a9820a3077f9a3d09a4dc8d367349335a1895ec7
Muhammad Ilyas <101690101+ilyas4225@users.noreply.github.com>
2023-02-28 09:45:30 +0500

    update

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-python train.py --data /home/msiddi01/ImageNet_Classes_2/Volley_Basket/ --class_labels 0 1 --save EfficientNetB0_ImNetClass01
-python train.py --data /media/abdul/8266fdc5-61f6-47d6-9e82-aa0e56f4b319/Ilyas/classification/Benchmark-TinyNAS_EfficientNet_ImNet_Vehicles/data/food-101.tar.gz --dataset Food101 --epochs 3  --save EfficientNetB0_food101
+python train.py --dataset DTD --batch_size 16 --epochs 50 --save EfficientNetB0_DTD
+
 
